3715217368d4b43482ad223784cffd270c261734
alexandr <sasha.pinsk2003@gmail.com>
2023-01-21 02:11:15 +0300

    add passenger test

---
+++ BusinessService/src/test/java/com/company/innowise/airticketsapp/businessservice/service/PassengerServiceTest.java
@@ -0,0 +1,48 @@
+package com.company.innowise.airticketsapp.businessservice.service;
+
+import com.company.innowise.airticketsapp.businessservice.model.Passenger;
+import com.company.innowise.airticketsapp.businessservice.model.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class PassengerServiceTest {
+
+    private final PassengerService passengerService;
+
+    @Autowired
+    public PassengerServiceTest(PassengerService passengerService) {
+        this.passengerService = passengerService;
+    }
+
+    @Test
+    void getAll() {
+
+    }
+
+    @Test
+    void testGetAll() {
+    }
+
+    @Test
+    void getPassenger() {
+        Passenger passenger = new Passenger();
+        passenger.setBirthdate(LocalDate.now());
+        passenger.setFirstname("alexandr");
+        passenger.setLastname("kaminskiy");as
+        passengerService.addPassenger(new Passenger("sasha@gmail.com"))
+    }
+
+    @Test
+    void addPassenger() {
+    }
+
+    @Test
+    void deletePassenger() {
+    }
+}
---
+++ BusinessService/src/test/java/com/company/innowise/airticketsapp/businessservice/service/TicketServiceTest.java
@@ -0,0 +1,30 @@
+package com.company.innowise.airticketsapp.businessservice.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TicketServiceTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void testGetAll() {
+    }
+
+    @Test
+    void getTicket() {
+    }
+
+    @Test
+    void addTicket() {
+    }
+
+    @Test
+    void deleteTicket() {
+    }
+}
