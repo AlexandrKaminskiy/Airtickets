ccb1106c9709e9a9da89b582a000dff7a8867923
alexandr <sasha.pinsk2003@gmail.com>
2023-02-14 13:54:05 +0300

    add filters, user details service and security configuration

--- BusinessService/src/main/java/com/company/innowise/airticketsapp/businessservice/config/WebConfiguration.java
+++ BusinessService/src/main/java/com/company/innowise/airticketsapp/businessservice/config/WebConfiguration.java
@@ -1,14 +1,33 @@
 package com.company.innowise.airticketsapp.businessservice.config;
 
+import com.company.innowise.airticketsapp.businessservice.security.JwtFilter;
+import com.company.innowise.airticketsapp.businessservice.security.JwtUtils;
+import com.company.innowise.airticketsapp.businessservice.security.PassengerDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfiguration {
+
+    private final JwtFilter jwtFilter;
+    private final JwtUtils jwtUtils;
+    private final PassengerDetailsService clientDetailsService;
 
     @Bean
     public Docket api() {
@@ -19,5 +38,34 @@ public class WebConfiguration {
                 .build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/sanya").authenticated()
+                        .anyRequest().permitAll()
+                )
+
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors().disable()
+                .authenticationManager(authenticationManager())
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(clientDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(authProvider);
+    }
 }
