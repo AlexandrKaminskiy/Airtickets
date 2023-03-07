0502b25912b990d5e8e91dff7c9ae36df2355422
alexandr <sasha.pinsk2003@gmail.com>
2023-03-07 13:00:00 +0300

    remove redundant imports

--- AuditService/src/main/java/com/company/innowise/airticketsapp/auditservice/AuditServiceApplication.java
+++ AuditService/src/main/java/com/company/innowise/airticketsapp/auditservice/AuditServiceApplication.java
@@ -1,13 +1,7 @@
 package com.company.innowise.airticketsapp.auditservice;
 
-import com.company.innowise.airticketsapp.auditservice.model.Activity;
-import com.company.innowise.airticketsapp.auditservice.model.UserActivity;
-import com.company.innowise.airticketsapp.auditservice.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @SpringBootApplication
 public class AuditServiceApplication {
