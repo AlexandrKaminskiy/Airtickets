0c13fc1f266a0ec1781e930cc90083a3a0d29032
alexandr <sasha.pinsk2003@gmail.com>
2023-03-07 15:39:28 +0300

    add feign client

--- BusinessService/src/main/java/com/company/innowise/airticketsapp/businessservice/BusinessServiceApplication.java
+++ BusinessService/src/main/java/com/company/innowise/airticketsapp/businessservice/BusinessServiceApplication.java
@@ -2,10 +2,10 @@ package com.company.innowise.airticketsapp.businessservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableFeignClients
+@SpringBootApplication
 public class BusinessServiceApplication {
 
     public static void main(String[] args) {
--- BusinessService/src/main/java/com/company/innowise/airticketsapp/businessservice/client/ClientInfoExtractor.java
+++ BusinessService/src/main/java/com/company/innowise/airticketsapp/businessservice/client/ClientInfoExtractor.java
@@ -2,22 +2,22 @@ package com.company.innowise.airticketsapp.businessservice.client;
 
 import com.company.innowise.airticketsapp.businessservice.dto.Activity;
 import com.company.innowise.airticketsapp.businessservice.dto.UserInfo;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient
+@FeignClient(name = "user-audit", url = "${auditHost}")
 public interface ClientInfoExtractor {
 
-    @GetMapping("localhost:8081/user/{username}")
+    @GetMapping("/user/{username}")
     List<UserInfo> getUserInfo(@PathVariable String username,
                                @RequestParam Integer page,
                                @RequestParam Integer size);
 
-    @GetMapping("localhost:8081/activity/{activity}")
+    @GetMapping("/activity/{activity}")
     List<UserInfo> getActivityInfo(@PathVariable Activity activity,
                                    @RequestParam Integer page,
                                    @RequestParam Integer size);
