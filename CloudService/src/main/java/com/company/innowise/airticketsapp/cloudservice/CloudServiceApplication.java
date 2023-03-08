5a806e56e9188463fb6ef6406f9ecc7babbd3764
alexandr <sasha.pinsk2003@gmail.com>
2023-03-08 16:36:24 +0300

    add eureka server

--- CloudService/src/main/java/com/company/innowise/airticketsapp/cloudservice/CloudServiceApplication.java
+++ CloudService/src/main/java/com/company/innowise/airticketsapp/cloudservice/CloudServiceApplication.java
@@ -2,7 +2,9 @@ package com.company.innowise.airticketsapp.cloudservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class CloudServiceApplication {
 
